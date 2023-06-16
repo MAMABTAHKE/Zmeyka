2d51e986bb365a0c427ae23a4350bf16468c6087
MAMABTAHKE <136102475+MAMABTAHKE@users.noreply.github.com>
2023-06-16 17:59:32 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Data:
+    money: int = 0
+    clicks: int = 0
+    money_per_click: int = 1
+
+
+def main() -> None:
+    ...
+
+
+if __name__ == '__main__':
+    main()
